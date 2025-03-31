8d95117a18bef82e7dc7de22993fca34e4c445b3
khushali <khushalidesai2271@gmail.com>
2025-03-31 19:37:12 +0530

    changed promt

--- Skill_Matrix_Gemini/app/routes.py
+++ Skill_Matrix_Gemini/app/routes.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 from app.models import SkillRequest
 from app.services import generate_skill_recommendation
+from .models import Skill_req
 
 router = APIRouter()
 
 @router.post("/recommend-skills")
 def recommend_skills(request: SkillRequest):
-    recommended = generate_skill_recommendation(request.Role, request.NumberOfRecommendations, request.Skills)
-    return {"recommended_skills": recommended}
+    skill= Skill_req(
+        role=request.Role,
+        number=request.NumberOfRecommendations,
+        skills=str(request.Skills)
+    )
+    recommended = generate_skill_recommendation(skill)
+    return recommended
