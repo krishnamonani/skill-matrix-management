c35c5279116507f874dfa8fc3303960c691e190f
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2025-04-03 13:08:07 +0530

    Roles add in ABP Table

---
+++ SkillMatrixManagement/src/SkillMatrixManagement.Domain/CustomDataSeeding/AbpRoleSeedingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Identity;
+using Volo.Abp.Uow;
+
+namespace SkillMatrixManagement.CustomDataSeeding
+{
+    public class AbpRoleSeedingService :IDataSeedContributor, ITransientDependency
+    {
+        private readonly IIdentityRoleRepository _roleRepository;
+        private readonly IdentityRoleManager _roleManager;
+
+        public AbpRoleSeedingService(IIdentityRoleRepository roleRepository, IdentityRoleManager roleManager)
+        {
+            _roleRepository = roleRepository;
+            _roleManager = roleManager;
+        }
+
+        [UnitOfWork]
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            await AddRoleIfNotExistsAsync("manager");
+            await AddRoleIfNotExistsAsync("hr");
+            await AddRoleIfNotExistsAsync("employee");
+        }
+
+        private async Task AddRoleIfNotExistsAsync(string roleName)
+        {
+            var existingRole = await _roleRepository.FindByNormalizedNameAsync(roleName.ToUpper());
+            if (existingRole == null)
+            {
+                var newRole = new IdentityRole(Guid.NewGuid(), roleName);
+                await _roleManager.CreateAsync(newRole);
+            }
+        }
+    }
+}
