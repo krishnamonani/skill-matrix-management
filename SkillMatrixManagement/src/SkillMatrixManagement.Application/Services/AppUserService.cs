cd5738404d9b1d947b2150b3abbb30137fef0fbf
SusavanMondal <susovanmondal550@gmail.com>
2025-04-07 03:21:57 +0530

    You can now sign up and log in with Google for a faster and more secure experience. No more passwords—just one click and you're in!

--- SkillMatrixManagement/src/SkillMatrixManagement.Application/Services/AppUserService.cs
+++ SkillMatrixManagement/src/SkillMatrixManagement.Application/Services/AppUserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SkillMatrixManagement.DTOs.Shared;
 using SkillMatrixManagement.DTOs.UserDTO;
@@ -23,6 +24,7 @@ namespace SkillMatrixManagement.Services
         private readonly IIdentityUserRepository _identityUserRepository;
         private readonly IdentityUserManager _identityUserManager;
         private readonly IMapper _mapper;
+
 
         public AppUserService(IUserRepository userRepository,
                               IDepartmentRepository departmentRepository, 
