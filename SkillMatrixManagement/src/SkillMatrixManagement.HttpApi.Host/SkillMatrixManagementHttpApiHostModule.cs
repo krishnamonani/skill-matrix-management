e1bfc0f1d12b96a6c73043216d269d92d890679b
Rishav Das <iamrishavdas@gmail.com>
2025-04-12 20:53:27 +0530

    Remove: authentication

--- SkillMatrixManagement/src/SkillMatrixManagement.HttpApi.Host/SkillMatrixManagementHttpApiHostModule.cs
+++ SkillMatrixManagement/src/SkillMatrixManagement.HttpApi.Host/SkillMatrixManagementHttpApiHostModule.cs
@@ -146,21 +146,21 @@ public class SkillMatrixManagementHttpApiHostModule : AbpModule
         });
 
         // Cookie Settings for SameSite issue and expiration
-        Configure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, options =>
-        {
-            options.Cookie.SameSite = SameSiteMode.None;
-            //options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // it will ensure the cookie is transfer only through the https
-            options.ExpireTimeSpan = TimeSpan.FromDays(14); // Set expiration to 14 days
-            options.SlidingExpiration = true; // Optional: Renews the cookie if accessed within the expiration window
-        });
+        //Configure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, options =>
+        //{
+        //    options.Cookie.SameSite = SameSiteMode.None;
+        //    //options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // it will ensure the cookie is transfer only through the https
+        //    options.ExpireTimeSpan = TimeSpan.FromDays(14); // Set expiration to 14 days
+        //    options.SlidingExpiration = true; // Optional: Renews the cookie if accessed within the expiration window
+        //});
 
-        Configure<CookieAuthenticationOptions>(IdentityConstants.TwoFactorRememberMeScheme, options =>
-        {
-            options.Cookie.SameSite = SameSiteMode.None;
-            //options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // it will ensure the cookie is transfer only through the https
-            options.ExpireTimeSpan = TimeSpan.FromDays(30); // Set expiration to 30 days for "Remember Me"
-            options.SlidingExpiration = true; // Optional: Renews the cookie if accessed within the expiration window
-        });
+        //Configure<CookieAuthenticationOptions>(IdentityConstants.TwoFactorRememberMeScheme, options =>
+        //{
+        //    options.Cookie.SameSite = SameSiteMode.None;
+        //    //options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // it will ensure the cookie is transfer only through the https
+        //    options.ExpireTimeSpan = TimeSpan.FromDays(30); // Set expiration to 30 days for "Remember Me"
+        //    options.SlidingExpiration = true; // Optional: Renews the cookie if accessed within the expiration window
+        //});
     }
 
     private void ConfigureAuthentication(ServiceConfigurationContext context)
