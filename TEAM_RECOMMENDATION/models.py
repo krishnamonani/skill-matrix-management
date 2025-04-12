cc46b23d6df0d2b6722d6f43df2062fcc74765fb
preksharai10 <preksharai1008@gmail.com>
2025-04-12 15:09:43 +0530

    updated

--- TEAM_RECOMMENDATION/models.py
+++ TEAM_RECOMMENDATION/models.py
@@ -1,41 +1,19 @@
-# from pydantic import BaseModel
-# from typing import List, Dict
-
-# class SkillLevel(BaseModel):
-#     skills: Dict[str, str]
-
-# class Employee(BaseModel):
-#     id: str
-#     firstName: str
-#     lastName: str
-#     userName: str
-#     email: str
-#     experience: int
-#     department: str
-#     designation: str
-#     skills: List[str]
-#     proficiencies: List[str]
-#     projectStatus: str
-
-# class TeamRecommendationRequest(BaseModel):
-#     description: str  # Extracted from SRS or entered directly
-#     employees: List[Employee]  # Data from dev backend
 from pydantic import BaseModel
 from typing import List
 
 class Employee(BaseModel):
-    id: str
-    firstName: str
-    lastName: str
-    userName: str
-    email: str
-    experience: int
-    department: str
-    designation: str
-    skills: List[str]
-    proficiencies: List[str]
-    projectStatus: str
+    Id: str
+    FirstName: str
+    LastName: str
+    UserName: str
+    Email: str
+    Experience: int
+    Department: str
+    Designation: str
+    Skills: List[str]
+    Proficiencies: List[str]
+    ProjectStatus: str
 
 class TeamRecommendationRequest(BaseModel):
-    description: str
-    employees: List[Employee]
+    Description: str
+    Employees: List[Employee]
