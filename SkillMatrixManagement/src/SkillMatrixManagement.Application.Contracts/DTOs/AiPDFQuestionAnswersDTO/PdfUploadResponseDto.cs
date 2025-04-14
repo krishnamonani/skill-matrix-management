09aa2f69b5c3faba4ca741c6f9a256e75d4c75be
Rishav Das <iamrishavdas@gmail.com>
2025-04-14 18:09:32 +0530

    Add: pdf qna

---
+++ SkillMatrixManagement/src/SkillMatrixManagement.Application.Contracts/DTOs/AiPDFQuestionAnswersDTO/PdfUploadResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillMatrixManagement.DTOs.AiPDFQuestionAnswersDTO
+{
+    public class PdfUploadResponseDto
+    {
+        public string message { get; set; }
+    }
+}
